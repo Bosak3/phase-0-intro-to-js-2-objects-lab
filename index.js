25a755aad65881d7336b70cc02af3364099e1592
Adam <bosak3@gmail.com>
2021-10-14 19:37:13 -0500

    Wrote another set of code for practice for Phase-1

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
 // Write your solution in this file!
+// let employee = {
+//     name: 'Sam',
+//     streetAddress: '11 Broadway'
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     const newEmployee = {...employee}
+//     newEmployee[key] = value;
+//     return newEmployee;
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee[key] = value
+//     return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key){
+//     const newEmployee = {...employee}
+//     delete newEmployee.name;
+//     return newEmployee;
+
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee.name;
+//     return employee;
+// }
+
+
+// For Phase-1 practice
 let employee = {
-    name: 'Sam',
-    streetAddress: '11 Broadway'
+    name: 'BoJangles',
+    streetAddress: '22 Jump Street',
 }
 
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newEmployee = {...employee}
-    newEmployee[key] = value;
-    return newEmployee;
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+
+    newObj[key] = value;
+
+    return newObj;
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key] = value
-    return employee;
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+     return employee;
 }
 
-function deleteFromEmployeeByKey(employee, key){
+function deleteFromEmployeeByKey(employee, key) {
     const newEmployee = {...employee}
     delete newEmployee.name;
     return newEmployee;
-
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee.name;
     return employee;
 }
+
